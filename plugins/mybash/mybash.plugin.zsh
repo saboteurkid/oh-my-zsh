5675592cd20c7ef6a5d5aac4460b89e1128002f1
Saboteur Kid <liemle.softdev@gmail.com>
2016-10-31 22:18:07 +0700

    Add curl_info command

--- plugins/mybash/mybash.plugin.zsh
+++ plugins/mybash/mybash.plugin.zsh
@@ -1,1 +1,3 @@
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+alias curl_info="curl -s -w '\nIP: %{remote_ip}, Time: %{time_total}, HTTP status: %{http_code} \n' "
