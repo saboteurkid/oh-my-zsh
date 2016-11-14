5b649c75b304c696a49a3a03679c1d0ea688a0ee
liemlhd <liemlhd@phabricator.zapps.vn>
2016-11-14 09:30:19 +0700

    Add get current timestamp.

--- plugins/mybash/mybash.plugin.zsh
+++ plugins/mybash/mybash.plugin.zsh
@@ -1,4 +1,5 @@
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias curl_info="curl -s -w '\nIP: %{remote_ip}, Time: %{time_total}, HTTP status: %{http_code} \n' "
-alias curl_json="curl -H 'Content-type: application/json' "
+alias curl_json="curl -H 'Content-type: application/json' -X POST  "
+alias current_timemillis="date +%s%N | cut -b1-13"
