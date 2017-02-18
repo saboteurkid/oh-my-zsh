a495b6067606f3d3a42147d75a15854ec9baef22
liemlhd <liemlhd@vng.com.vn>
2017-02-18 15:02:34 +0700

    Add new commands.

--- plugins/mybash/mybash.plugin.zsh
+++ plugins/mybash/mybash.plugin.zsh
@@ -4,4 +4,7 @@ alias curl_info="curl -s -w '\nIP: %{remote_ip}, Time: %{time_total}, HTTP statu
 alias curl_json="curl -H 'Content-type: application/json' -X POST  "
 alias current_timemillis="date +%s%N | cut -b1-13"
 
-alias pkill="ps -x | egrep -i "" | awk '{print $1}' | xargs kill -9 $1"
+alias pkill="ps -x | egrep -i "" | awk '{print $2}' | xargs kill -9 $1"
+alias gitls-del="git ls-files -d"
+alias gitls-new="git ls-files -o --exclude-standard"
+alias grepPort="lsof -i -n -P | grep TCP | grep "
